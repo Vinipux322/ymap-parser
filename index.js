f3a248652361eed83a8478f700f35ef7636283bc
Vinipux322 <36007766+Vinipux322@users.noreply.github.com>
2020-08-04 05:47:42 +0300

    off debug shit, add crop number for coords, remove all ;

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const fs = require('fs');
-const xml2js = require('xml2js');
-const readlineSync = require('readline-sync');
+const fs = require('fs')
+const xml2js = require('xml2js')
+const readlineSync = require('readline-sync')
 
-let path = '';
+let path = ''
+
+function cropNumber(number){
+  number = number.replace(/0+$/, '')
+  number = number.replace(/\.+$/, '')
+  return number
+}
 
 function isPathValid(path){
   try {
     fs.accessSync(path)
-    return true;
+    return true
   } catch(e) {
     console.log('Path not valid.')
-    return false;
+    return false
   }
 }
 
 while (!path || !isPathValid(path)) {
-  path = readlineSync.question('Wait for path to folder: ');
+  path = readlineSync.question('Wait for path to folder: ')
 }
 
 const parser = new xml2js.Parser()
@@ -30,7 +36,7 @@ for (const file of files) {
   parser.parseString(data, (err, result) => {
     for (const key in result.CMapData.entities[0].Item) {
       const prop = result.CMapData.entities[0].Item[key]
-      console.log(prop.position[0].$)
+      // console.log(prop.position[0].$)
       const name = prop.archetypeName[0].toLowerCase()
       if (!parsedData[name]) {
         parsedData[name] = []
@@ -46,11 +52,11 @@ for (const prop_name in parsedData) {
   const coords = parsedData[prop_name]
   for (const position of coords) {
     console.log
-    const newText = `{'${prop_name}', ${position.x}, ${position.y}, ${position.z}},\n`
+    const newText = `{'${prop_name}', ${cropNumber(position.x)}, ${cropNumber(position.y)}, ${cropNumber(position.z)}},\n`
     fileContent += newText
   }
 }
 
-fs.writeFileSync('exportData.txt', fileContent);
+fs.writeFileSync('exportData.txt', fileContent)
 
 console.log('Done!')
